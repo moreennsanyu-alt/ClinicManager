ffa69b818b2c17678aaea21ca0eab79e5e62ab32
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-02-07 07:50:06 +0300

    Create App.xaml.cs

---
+++ Src/ClinicManager.Win/App.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+namespace ClinicManager.Win
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Logic for dependency injection or global error handling goes here
+        }
+    }
+}
