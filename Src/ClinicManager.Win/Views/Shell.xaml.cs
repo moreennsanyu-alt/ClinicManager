8689ba6c699cfa7ae888391d77b4ca2bff525e78
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-02-07 07:51:38 +0300

    Create Shell.xaml.cs

---
+++ Src/ClinicManager.Win/Views/Shell.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace ClinicManager.Win.Views
+{
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}
