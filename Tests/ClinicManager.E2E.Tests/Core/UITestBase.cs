ab2418c70dfc305385da1d7b1e66b7738934f93b
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-02-09 04:13:51 +0300

    Create Core/UITestBase.cs

---
+++ Tests/ClinicManager.E2E.Tests/Core/UITestBase.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Text.RegularExpressions;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Input;
+using FlaUI.Core.WindowsAPI;
+using FlaUI.TestUtilities;
+using FlaUI.UIA3;
+using NUnit.Framework;
+using OperatingSystem = FlaUI.Core.Tools.OperatingSystem;
+
+namespace ClinicManager.E2E.Tests.Core;
+
+public class UITestBase: FlaUITestBase
+{
+    protected override AutomationBase GetAutomation()
+    {
+        return new UIA3Automation();
+    }
+    
+    protected override Application StartApplication()
+        {
+            if (OperatingSystem.IsWindows10())
+            {
+                // Use the store application on those systems
+                return Application.LaunchStoreApp("Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            }
+            if (OperatingSystem.IsWindowsServer2016() || OperatingSystem.IsWindowsServer2019())
+            {
+                // The calc.exe on this system is just a stub which launches win32calc.exe
+                return Application.Launch("win32calc.exe");
+            }
+            return Application.Launch("calc.exe");
+        }
+    }
+}
