9ec972a3ef44f090e89b2d0e4373e25a6f05598a
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-02-09 10:20:44 +0300

    Create SampleTest.cs

---
+++ Tests/ClinicManager.E2E.Tests/SampleTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using CalculatorApp; // Reference the namespace of the class to be tested
+
+namespace CalculatorApp.Tests
+{
+    [TestFixture] // Optional in NUnit 3+, but good for clarity/compatibility
+    public class CalculatorTests
+    {
+
+        [Test] // Marks a method as a test method
+        public void Test(){}
+
+    }
+}
