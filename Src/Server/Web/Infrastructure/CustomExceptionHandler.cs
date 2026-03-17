0ff8cdc0864e9a5a4cf71d3a75ede4e6b3b3fd2f
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-03-17 22:53:12 +0300

    Update CustomExceptionHandler.cs

--- Src/Server/Web/Infrastructure/CustomExceptionHandler.cs
+++ Src/Server/Web/Infrastructure/CustomExceptionHandler.cs
@@ -1,4 +1,4 @@
-using CleanArchitecture.Application.Common.Exceptions;
+using ClinicManager.Application.Common.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
