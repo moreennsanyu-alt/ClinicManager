6786afbab98e5e24f7e82f791d57948747bdc99e
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-03-30 13:28:31 +0300

    Last Sync: 2026-03-30 13:28 (Mobile)

--- Src/Desktop/ClinicManager.Win/App.xaml.cs
+++ Src/Desktop/ClinicManager.Win/App.xaml.cs
@@ -3,17 +3,23 @@ using Prism.DryIoc;
 using System.Windows;
 using ClinicManager.Win.Views;
 using ClinicManager.Win.Application.Lifetime;
+using SingleInstanceCore;
 
 namespace ClinicManager.Win;
 
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class ClinicManagerApp : PrismApplication
+    public partial class ClinicManagerApp : PrismApplication, ISingleInstance
     {
         static ClinicManagerApp()
         {
         }
+        
+        public void OnInstanceInvoked(string[] args)
+        {
+		    // What to do with the args another instance has sent
+	    }
         
         protected override Window CreateShell()
         {
@@ -33,14 +39,21 @@ namespace ClinicManager.Win;
         
         protected override void OnStartup(StartupEventArgs e)
         {
-
-            var first = ApplicationActivator.LaunchOrReturn(otherInstance => { MessageBox.Show("got data"); }, e.Args);
-            if (!first)
-            {
-                Shutdown();
-            }
-
+            bool isFirstInstance = this.InitializeAsFirstInstance("soheilkd_ExampleIPC");
+		    if (!isFirstInstance)
+		    {
+			    //If it's not the first instance, arguments are automatically passed to the first instance
+			    //OnInstanceInvoked will be raised on the first instance
+			    //You may shut down the current instance
+			    Current.Shutdown();
+		    }
 
             base.OnStartup(e);
+        }
+        
+        protected override void OnExit(ExitEventArgs e)
+        {
+            SingleInstance.Cleanup();
+            base.OnExit(e);
         }
     }
