7e5e9175db5fe4686937ba07e32ddaf645b75dce
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-03-30 14:31:45 +0300

    Last Sync: 2026-03-30 14:31 (Mobile)

---
+++ Src/Desktop/ClinicManager.Win/Application/Lifetime/Payload.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ClinicManager.Win.Application.Lifetime
+{
+    [DataContract]
+    internal class Payload
+    {
+        /// <summary>
+        ///     A list of command line arguments.
+        /// </summary>
+        [DataMember]
+        public List<string> CommandLineArguments { get; set; } = new List<string>();
+    }
+}
