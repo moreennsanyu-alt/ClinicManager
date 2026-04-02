2eb313ab96e8f6fb9ee85eabbba25fcf0fa141f9
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-04-02 07:02:07 +0300

    Last Sync: 2026-04-02 07:02 (Mobile)

--- Src/Server/Web/Migrations/20240301000000_CreateIdentitySchema.Designer.cs
+++ Src/Server/Web/Migrations/20240301000000_CreateIdentitySchema.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using using ClinicManager.Data;
+using ClinicManager.Data;
 
 #nullable disable
 
