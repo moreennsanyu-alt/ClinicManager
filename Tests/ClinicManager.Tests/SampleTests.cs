97b018778e21085dab78cf2d5a073164bbe53fd4
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-04-03 09:18:08 +0300

    Last Sync: 2026-04-03 09:18 (Mobile)

---
+++ Tests/ClinicManager.Tests/SampleTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ClinicManager.Tests;
+
+public class CalculatorTests
+{
+    // [Fact] is used for tests that are always true (no parameters)
+    [Fact]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+
+    }
+}
---
+++ Tests/ClinicManager.Win.Tests/SampleTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ClinicManager.Win.Tests;
+
+public class CalculatorTests
+{
+    // [Fact] is used for tests that are always true (no parameters)
+    [Fact]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+
+    }
+}
