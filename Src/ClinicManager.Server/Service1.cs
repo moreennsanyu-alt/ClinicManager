e1506e547ed5bbd9dfc37dd33cccb6b126aca1c6
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-04-03 08:31:10 +0300

    Last Sync: 2026-04-03 08:31 (Mobile)

--- Src/ClinicManager.Server/Service1.cs
+++ Src/ClinicManager.Server/Service1.cs
@@ -11,13 +11,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 
-namespace ClinicManager.Server
+namespace WindowsService.NET
 {
     public partial class Service1 : ServiceBase
     {
-        Timer Timer = new Timer();
-        int Interval = 10000; // 10000 ms = 10 second  
-
+        
         public Service1()
         {
             InitializeComponent();
@@ -26,41 +24,15 @@ namespace ClinicManager.Server
 
         protected override void OnStart(string[] args)
         {
-            WriteLog("Service has been started");
-            Timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
-            Timer.Interval = Interval;
-            Timer.Enabled = true;
+            
         }
 
-        private void OnElapsedTime(object source, ElapsedEventArgs e)
-        {
-            WriteLog(String.Format("{0} ms elapsed.", Interval));
-        }
+        
 
         protected override void OnStop()
         {
-            Timer.Stop();
-            WriteLog("Service has been stopped.");
-        }
+            }
 
-        public void WriteLog(string logMessage, bool addTimeStamp = true)
-        {
-            var path = AppDomain.CurrentDomain.BaseDirectory;
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-
-            var filePath = String.Format("{0}\\{1}_{2}.txt",
-                path,
-                ServiceName,
-                DateTime.Now.ToString("yyyyMMdd", CultureInfo.CurrentCulture)
-                );
-
-            if (addTimeStamp)
-                logMessage = String.Format("[{0}] - {1}\r\n",
-                    DateTime.Now.ToString("HH:mm:ss", CultureInfo.CurrentCulture),
-                    logMessage);
-
-            File.AppendAllText(filePath, logMessage);
-        }
+        
     }
 }
--- Src/ClinicManager.Win/Program.cs
+++ Src/ClinicManager.Win/Program.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
-using WindowsFormsForm;
 
 namespace ClinicManager.Win
 {
