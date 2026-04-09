a97e78bfbd933289585929748f41271ffc3d148e
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-04-09 04:53:45 +0300

    Last Sync: 2026-04-09 04:53 (Mobile)

--- Src/ClinicManager.WFServer/Service1.cs
+++ Src/ClinicManager.WFServer/Service1.cs
@@ -15,9 +15,7 @@ namespace ClinicManager.WFServer
 {
     public partial class Service1 : ServiceBase
     {
-        Timer Timer = new Timer();
-        int Interval = 10000; // 10000 ms = 10 second  
-
+        
         public Service1()
         {
             InitializeComponent();
@@ -26,41 +24,19 @@ namespace ClinicManager.WFServer
 
         protected override void OnStart(string[] args)
         {
-            WriteLog("Service has been started");
-            Timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
-            Timer.Interval = Interval;
-            Timer.Enabled = true;
-        }
+  }
 
         private void OnElapsedTime(object source, ElapsedEventArgs e)
         {
-            WriteLog(String.Format("{0} ms elapsed.", Interval));
+           // WriteLog(String.Format("{0} ms elapsed.", Interval));
         }
 
         protected override void OnStop()
         {
             Timer.Stop();
-            WriteLog("Service has been stopped.");
+            //WriteLog("Service has been stopped.");
         }
 
-        public void WriteLog(string logMessage, bool addTimeStamp = true)
-        {
-            var path = AppDomain.CurrentDomain.BaseDirectory;
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-
-            var filePath = String.Format("{0}\\{1}_{2}.txt",
-                path,
-                ServiceName,
-                DateTime.Now.ToString("yyyyMMdd", CultureInfo.CurrentCulture)
-                );
-
-            if (addTimeStamp)
-                logMessage = String.Format("[{0}] - {1}\r\n",
-                    DateTime.Now.ToString("HH:mm:ss", CultureInfo.CurrentCulture),
-                    logMessage);
-
-            File.AppendAllText(filePath, logMessage);
-        }
+        
     }
 }
