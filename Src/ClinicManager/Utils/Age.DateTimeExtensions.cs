e05e7459c47f893485173fca4bd05e3945780426
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-04-09 05:44:01 +0300

    Last Sync: 2026-04-09 05:43 (Mobile)

---
+++ Src/ClinicManager/Utils/Age.DateTimeExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace ClinicManager.Utils
+{
+    /// <summary>
+    /// Contains <see cref="AgeCalculator"/>'s <see cref="DateTime"/> extension methods.
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Calculates the age between this and the specified <see cref="DateTime"/> instance.
+        /// </summary>
+        /// <inheritdoc cref="Age(DateTime,DateTime,bool)"/>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when this <see cref="DateTime"/> instance is considered greater than <paramref name="toDate"/>.</exception>
+        public static Age CalculateAge(
+            this DateTime fromDate,
+            DateTime toDate,
+            bool isFeb28AYearCycleForLeapling = false)
+        {
+            if (fromDate > toDate) throw new ArgumentOutOfRangeException(
+                nameof(fromDate),
+                $"This {nameof(DateTime)} must be less or equal to '{nameof(toDate)}'.");
+
+            return new Age(fromDate, toDate, isFeb28AYearCycleForLeapling);
+        }
+    }
+}
