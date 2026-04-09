1a751a537a4acb8f4f478d736d9e8c3c6a725787
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-04-09 04:31:33 +0300

    Last Sync: 2026-04-09 04:31 (Mobile)

---
+++ Tests/ClinicManager.E2E.Tests/SampleTests.cs
@@ -0,0 +1,13 @@
+
+
+namespace ClinicManager.E2E.Tests;
+
+public class CalculatorTests
+{
+    // [Fact] is used for tests that are always true (no parameters)
+    [Test]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+
+    }
+}
---
+++ Tests/ClinicManager.Tests/SampleTests.cs
@@ -0,0 +1,12 @@
+
+namespace ClinicManager.Win.Tests;
+
+public class CalculatorTests
+{
+    // [Fact] is used for tests that are always true (no parameters)
+    [Test]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+
+    }
+}
