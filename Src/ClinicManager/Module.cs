44a1f172f282ce05a33159fbb56842e86a2d8463
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-04-21 16:57:37 +0300

    Update Module.cs

--- Src/ClinicManager/Module.cs
+++ Src/ClinicManager/Module.cs
@@ -1,1 +1,12 @@
-Module.cs
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp;
+namespace ClinicManager;
+
+public class UnitTest1
+{
+    [Fact]
+    public void AddTest()
+    {
+       // Assert.Equal(5, Class1.Add(2, 3));
+    }
+}
