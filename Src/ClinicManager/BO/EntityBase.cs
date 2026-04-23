cb3739c639fd5b0188494037672c6e5791083205
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-04-23 16:01:51 +0300

    Add IEmployee interface with validation rules

--- Src/ClinicManager/BO/EntityBase.cs
+++ Src/ClinicManager/BO/EntityBase.cs
@@ -1,1 +1,20 @@
+using System;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
 
+namespace TestValidation.Module {
+    [DomainComponent, DefaultClassOptions, ImageName("BO_Employee")]
+    public interface IEmployee {
+        [RuleRequiredField(EmployeeValidationRules.EmployeeNameIsRequired, DefaultContexts.Save)]
+        string Name { get; set; }
+        [RuleValueComparison(EmployeeValidationRules.EmployeeIsAdult, DefaultContexts.Save, 
+            ValueComparisonType.GreaterThanOrEqual, 18)]
+        int Age { get; set;}
+    }
+    public class EmployeeValidationRules {
+        public const string EmployeeNameIsRequired = "EmployeeNameIsRequired";
+        public const string EmployeeIsAdult = "EmployeeIsAdult";
+    }
+
+}
