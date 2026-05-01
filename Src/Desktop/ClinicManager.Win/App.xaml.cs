6befc17ea2fc1fd893f67615646b910bb56660c0
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-05-01 16:49:25 +0300

    Add Prism framework integration to App.xaml.cs

--- Src/Desktop/ClinicManager.Win/App.xaml.cs
+++ Src/Desktop/ClinicManager.Win/App.xaml.cs
@@ -1,1 +1,23 @@
+using Prism.Ioc;
+using Prism.DryIoc;
+using ClinicManager.Win.Views;
+using System.Windows;
 
+namespace ClinicManager.Win
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
