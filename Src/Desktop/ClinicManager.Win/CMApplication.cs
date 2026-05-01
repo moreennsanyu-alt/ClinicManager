cf61c2ee8092a9fb1ce79884b1c36248eb93241b
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-05-01 16:56:09 +0300

    Update CMApplication.cs

--- Src/Desktop/ClinicManager.Win/CMApplication.cs
+++ Src/Desktop/ClinicManager.Win/CMApplication.cs
@@ -1,1 +1,12 @@
+using Prism.Ioc;
+using Prism.DryIoc;
+using ClinicManager.Win.Views;
+using System.Windows;
 
+namespace ClinicManager.Win
+{
+    public abstract class CMApplication : PrismApplication
+    {
+        
+    }
+}
