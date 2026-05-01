a78b528b82b9243092eda64a110a2b620295c847
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-05-01 16:54:30 +0300

    Add Shell class for MainWindow interaction logic

--- Src/Desktop/ClinicManager.Win/Views/Shell.xaml.cs
+++ Src/Desktop/ClinicManager.Win/Views/Shell.xaml.cs
@@ -1,1 +1,15 @@
+using System.Windows;
 
+namespace ClinicManager.Win.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Shell : Window
+    {
+        public Shell
+        {
+            InitializeComponent();
+        }
+    }
+}
