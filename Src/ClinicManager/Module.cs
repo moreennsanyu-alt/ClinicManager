d0aaa52df1b23cb29009b4dff344f1b9bf58fc39
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-05-01 07:48:06 +0300

    Remove unused ScriptRecorder.Win namespace

--- Src/ClinicManager/Module.cs
+++ Src/ClinicManager/Module.cs
@@ -8,7 +8,6 @@ using DevExpress.ExpressApp.PivotChart.Win;
 using DevExpress.ExpressApp.PivotGrid.Win;
 using DevExpress.ExpressApp.ReportsV2;
 using DevExpress.ExpressApp.Scheduler.Win;
-using DevExpress.ExpressApp.ScriptRecorder.Win;
 using DevExpress.ExpressApp.TreeListEditors;
 using DevExpress.ExpressApp.TreeListEditors.Win;
 using DevExpress.ExpressApp.Updating;
