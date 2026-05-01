9e988e2d79ca1f84443d91fca03006431868c46b
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-05-01 20:58:16 +0300

    Add files via upload

---
+++ Src/Desktop.Tests/ClinicManager.E2E.Tests/Core/UITestBase.cs
@@ -0,0 +1,30 @@
+using FlaUI.Core;
+using FlaUI.Core.Capturing;
+using FlaUI.Core.Input;
+using FlaUI.Core.Tools;
+using FlaUI.UIA3;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace ClinicManager.E2E.Tests.Core;
+
+public abstract class UITestBase 
+{
+    static UITestBase()
+    {
+        NativeMethods.SetProcessDPIAware();
+        CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
+        Mouse.MovePixelsPerMillisecond = 2;
+        Retry.DefaultTimeout = TimeSpan.FromSeconds(5);
+        Retry.DefaultInterval = TimeSpan.FromMilliseconds(250);
+    }
+
+
+    private static class NativeMethods
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetProcessDPIAware();
+    }
+}
