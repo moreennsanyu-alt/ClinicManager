3b67a53dbf2dfad32dafac131b5539b78fa2c587
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-05-02 07:52:24 +0300

    Refactor AccountTest to use new namespace and base class
    Updated namespace and inheritance for AccountTest class.

--- Src/Desktop.Tests/ClinicManager.E2E.Tests/SampleTest.cs
+++ Src/Desktop.Tests/ClinicManager.E2E.Tests/SampleTest.cs
@@ -1,13 +1,15 @@
-namespace Bank
-{
-  using NUnit.Framework;
+using ClinicManager.E2E.Tests.Core
+using NUnit.Framework;
+
+namespace ClinicManager.E2E.Tests;
 
   [TestFixture]
-  public class AccountTest
+  public class AccountTest : UITestBase
   {
     [Test]
     public void TransferFunds()
     {
+    var fg = 4 / 0;
     }
   }
 }
