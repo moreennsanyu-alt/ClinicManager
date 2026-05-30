a34513f4cc33e2a20f837d50027b11548dd1abc8
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-05-30 06:51:23 +0300

    Last Sync: 2026-05-30 06:51 (Mobile)

---
+++ Src/Server.Tests/Application.FunctionalTests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Ardalis.GuardClauses;
+global using ClinicManager.Application.FunctionalTests.Infrastructure;
+global using ClinicManager.Shared;
+global using Moq;
+global using NUnit.Framework;
+global using Shouldly;
