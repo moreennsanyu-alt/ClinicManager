56936651cf37b2efd0c19a25276b9b89291735c1
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-05-30 07:50:10 +0300

    Last Sync: 2026-05-30 07:50 (Mobile)

--- Src/Server/Web/Services/CurrentUser.cs
+++ Src/Server/Web/Services/CurrentUser.cs
@@ -1,8 +1,8 @@
-﻿using System.Security.Claims;
+using System.Security.Claims;
 
-using CleanArchitecture.Application.Common.Interfaces;
+using ClinicManager.Application.Common.Interfaces;
 
-namespace CleanArchitecture.Web.Services;
+namespace ClinicManager.Web.Services;
 
 public class CurrentUser : IUser
 {
