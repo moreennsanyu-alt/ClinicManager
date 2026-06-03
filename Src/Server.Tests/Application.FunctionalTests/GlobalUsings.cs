c5f8aa29bfeddb05ec69f3db690e5db7ba013bd0
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-06-03 05:00:43 +0300

    Last Sync: 2026-06-03 05:00 (Mobile)

--- Src/Server.Tests/Application.FunctionalTests/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Ardalis.GuardClauses;
-global using ClinicManager.Application.FunctionalTests.Infrastructure;
-global using ClinicManager.Shared;
-global using Moq;
-global using NUnit.Framework;
-global using Shouldly;
--- Src/Server/Application/Common/Exceptions/ForbiddenAccessException.cs
+++
@@ -1,6 +0,0 @@
-namespace ClinicManager.Application.Common.Exceptions;
-
-public class ForbiddenAccessException : Exception
-{
-    public ForbiddenAccessException() : base() { }
-}
