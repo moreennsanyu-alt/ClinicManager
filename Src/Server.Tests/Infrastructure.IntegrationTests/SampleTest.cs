f81cb57ab7aff0b6d4488b4c0a96f61c78820473
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-06-05 01:49:58 +0300

    Create SampleTest class for unit testing
    Add initial unit test class with a test case.

---
+++ Src/Server.Tests/Infrastructure.IntegrationTests/SampleTest.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using System;
+
+namespace MyApp.Tests
+{
+    [TestFixture] // Identifies this class as containing unit tests
+    public class SampleTest
+    {
+        [Test] // Marks this method as a test case
+        public void Tedt1()
+        {
+        }
+
+ 
+    }
+}
