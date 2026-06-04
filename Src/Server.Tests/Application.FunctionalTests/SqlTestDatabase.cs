afa0c58f3d66c6f2e18710ec1a5410ef053caed8
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-06-05 01:00:59 +0300

    Update SqlTestDatabase.cs

--- Src/Server.Tests/Application.FunctionalTests/SqlTestDatabase.cs
+++ Src/Server.Tests/Application.FunctionalTests/SqlTestDatabase.cs
@@ -1,11 +1,11 @@
 ﻿using System.Data.Common;
-using CleanArchitecture.Infrastructure.Data;
+using ClinicManager.Infrastructure.Data;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Respawn;
 
-namespace CleanArchitecture.Application.FunctionalTests;
+namespace ClinicManager.Application.FunctionalTests;
 
 public class SqlTestDatabase : ITestDatabase
 {
