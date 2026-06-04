b5580cd34c91e82a448de88782719f32cecfede3
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-06-05 00:50:36 +0300

    Refactor namespace and using directives in tests

--- Src/Server.Tests/Application.FunctionalTests/TodoItems/Commands/UpdateTodoItemTests.cs
+++ Src/Server.Tests/Application.FunctionalTests/TodoItems/Commands/UpdateTodoItemTests.cs
@@ -1,9 +1,9 @@
-﻿using CleanArchitecture.Application.TodoItems.Commands.CreateTodoItem;
-using CleanArchitecture.Application.TodoItems.Commands.UpdateTodoItem;
-using CleanArchitecture.Application.TodoLists.Commands.CreateTodoList;
-using CleanArchitecture.Domain.Entities;
+﻿using ClinicManager.Application.TodoItems.Commands.CreateTodoItem;
+using ClinicManager.Application.TodoItems.Commands.UpdateTodoItem;
+using ClinicManager.Application.TodoLists.Commands.CreateTodoList;
+using ClinicManager.Domain.Entities;
 
-namespace CleanArchitecture.Application.FunctionalTests.TodoItems.Commands;
+namespace ClinicManager.Application.FunctionalTests.TodoItems.Commands;
 
 using static Testing;
 
