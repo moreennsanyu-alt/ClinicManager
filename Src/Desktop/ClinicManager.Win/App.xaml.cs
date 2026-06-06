52e424b1da1f338471cb2761c4a2114f5b07927b
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-06-06 04:36:54 +0300

    Update App.xaml.cs

--- Src/Desktop/ClinicManager.Win/App.xaml.cs
+++ Src/Desktop/ClinicManager.Win/App.xaml.cs
@@ -1,1 +1,25 @@
+using Prism.Ioc;
+using Prism.DryIoc;
+using Regions.Views;
+using System.Windows;
+using ClinicManager.Win.ViewModels;
+using ClinicManager.Win.Views;
 
+namespace ClinicManager.Win
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
