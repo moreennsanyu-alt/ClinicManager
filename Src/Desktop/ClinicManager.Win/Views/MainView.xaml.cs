73ff27adbadb8573cffef6b1e3f01c7bed630e67
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-06-06 05:07:01 +0300

    Add MainView.xaml.cs for MainView interaction logic

--- Src/Desktop/ClinicManager.Win/Views/MainView.xaml.cs
+++ Src/Desktop/ClinicManager.Win/Views/MainView.xaml.cs
@@ -1,1 +1,15 @@
+using System.Windows;
 
+namespace ClinicManager.Win.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : UserControl 
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
