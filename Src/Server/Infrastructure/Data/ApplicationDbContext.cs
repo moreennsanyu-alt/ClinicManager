1ae2594be28fe5ad7316c0ca1202c5301df91fa0
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-20 16:21:55 +0300

    Last Sync: 2026-09-20 16:21 (Mobile)

--- Src/Server/Infrastructure/Data/ApplicationDbContext.cs
+++
@@ -1,5 +0,0 @@
-﻿global using ClinicManager.Web.AcceptanceTests.Pages;
-global using Reqnroll.BoDi;
-global using Shouldly;
-global using Microsoft.Playwright;
-global using Reqnroll;
--- Src/Server/Web/Endpoints/Users.cs
+++
@@ -1,3 +0,0 @@
-global using Ardalis.GuardClauses;
-global using ClinicManager.Web.Infrastructure;
-global using MediatR;
