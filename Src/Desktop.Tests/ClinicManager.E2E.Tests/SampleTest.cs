6186f5eb019726526b5eb1f272fe0b8df6bca583
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-24 12:23:34 +0300

    Update SampleTest.cs

--- Src/Desktop.Tests/ClinicManager.E2E.Tests/SampleTest.cs
+++ Src/Desktop.Tests/ClinicManager.E2E.Tests/SampleTest.cs
@@ -8,7 +8,13 @@ namespace ClinicManager.E2E.Tests
         public void TestMethodName()
         {
             var shellWindow = GetShellWindow();
-            
+            shellWindow.SendButton.Click();
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            var path = TakeScreenShot("hello1");
+            TestContext.Current.AddAttachment(
+                            "hello1.png",
+                            File.ReadAllBytes(path),
+                            ".png"); 
         }
 
         
