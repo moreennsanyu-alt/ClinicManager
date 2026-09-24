71a9e049b9cbd47d0b8b72de5332d7fa0369b857
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-24 10:39:32 +0300

    Add global usings for WPF and DevExpress components

--- Src/Desktop/ClinicManager.Win/GlobalUsings.cs
+++ Src/Desktop/ClinicManager.Win/GlobalUsings.cs
@@ -6,6 +6,79 @@ global using ClinicManager.Win.Ioc;
 global using ClinicManager.Win.Net.Http;
 
 
+global using System.Windows;
+global using System.Windows.Controls;
+global using System.Windows.Controls.Primitives;
+global using System.Windows.Data;
+global using System.Windows.Documents;
+global using System.Windows.Input;
+global using System.Windows.Interop;
+global using System.Windows.Markup;
+global using System.Windows.Media;
+global using System.Windows.Media.Animation;
+global using System.Windows.Media.Effects;
+global using System.Windows.Media.Imaging;
+global using System.Windows.Navigation;
+global using System.Windows.Resources;
+
+// GlobalUsings.cs
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using CommunityToolkit.Mvvm.Messaging;
+
+global using Prism.Commands;
+global using Prism.Events;
+global using Prism.Ioc;
+global using Prism.Mvvm;
+global using Prism.Modularity;
+global using Prism.Regions;
+global using Prism.Services.Dialogs;
+global using DelegateCommand = Prism.Commands.DelegateCommand;
+global using BindableBase = Prism.Mvvm.BindableBase;
+
+global using DevExpress.Xpf.Core;
+global using DevExpress.Xpf.Editors;
+global using DevExpress.Xpf.LayoutControl;
+global using DevExpress.Xpf.Docking;
+global using DevExpress.Xpf.Bars;
+global using DevExpress.Xpf.Ribbon;
+global using DevExpress.Xpf.Grid;
+global using DevExpress.Xpf.PropertyGrid;
+global using DevExpress.Xpf.PivotGrid;
+global using DevExpress.Xpf.Scheduling;
+global using DevExpress.Xpf.Charts;
+global using DevExpress.Xpf.Gauges;
+global using DevExpress.Xpf.RichEdit;
+global using DevExpress.Xpf.PdfViewer;
+global using DevExpress.Xpf.Printing;
+global using DevExpress.Mvvm;
+global using DevExpress.Mvvm.DataAnnotations;
+global using DevExpress.Mvvm.CodeGenerators;
+
+global using DialogResult = Prism.Services.Dialogs.DialogResult;
+global using Application = System.Windows.Application;
+global using Button = System.Windows.Controls.Button;
+global using CheckBox = System.Windows.Controls.CheckBox;
+global using ComboBox = System.Windows.Controls.ComboBox;
+global using ContextMenu = System.Windows.Controls.ContextMenu;
+global using GroupBox = System.Windows.Controls.GroupBox;
+global using Label = System.Windows.Controls.Label;
+global using ListBox = System.Windows.Controls.ListBox;
+global using ListView = System.Windows.Controls.ListView;
+global using Menu = System.Windows.Controls.Menu;
+global using MenuItem = System.Windows.Controls.MenuItem;
+global using Panel = System.Windows.Controls.Panel;
+global using ProgressBar = System.Windows.Controls.ProgressBar;
+global using RadioButton = System.Windows.Controls.RadioButton;
+global using RichTextBox = System.Windows.Controls.RichTextBox;
+global using ScrollBar = System.Windows.Controls.Primitives.ScrollBar;
+global using TabControl = System.Windows.Controls.TabControl;
+global using TabItem = System.Windows.Controls.TabItem;
+global using TextBox = System.Windows.Controls.TextBox;
+global using ToolTip = System.Windows.Controls.ToolTip;
+global using TreeView = System.Windows.Controls.TreeView;
+global using UserControl = System.Windows.Controls.UserControl;
+
 
 
 
