7f58f62b85747ceff07a227d0098b7d596a43d98
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-24 08:49:41 +0300

    Update namespace for MessageBox class

--- Src/Desktop.Tests/ClinicManager.E2E.Tests/Core/SystemViews/MessageBox.cs
+++ Src/Desktop.Tests/ClinicManager.E2E.Tests/Core/SystemViews/MessageBox.cs
@@ -2,7 +2,7 @@
 using FlaUI.Core.AutomationElements;
 using FlaUI.Core.Definitions;
 
-namespace UITest.SystemViews;
+namespace ClinicManager.E2E.Tests.Core.SystemViews;
 
 public class MessageBox(FrameworkAutomationElementBase element) : Window(element)
 {
