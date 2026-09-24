53c70471c04d86389978acf32665cb354c0fc82e
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-24 09:53:46 +0300

    Add ShellWindow.xaml.cs file

---
+++ Src/Desktop/ClinicManager.Win/Features/Infrastructure/Shell/ShellWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace ClinicManager.Win.Features.Infrastructure.Shell;
+
+public partial class ShellWindow : Window
+{
+    public ShellWindow()
+    {
+        InitializeComponent();
+    }
+}
