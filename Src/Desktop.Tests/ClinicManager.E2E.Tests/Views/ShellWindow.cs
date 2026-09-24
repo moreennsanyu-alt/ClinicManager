78f2448943f3cad4948fe808e56cd49a57def2ad
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-24 12:04:49 +0300

    Add SendButton property to ShellWindow class

--- Src/Desktop.Tests/ClinicManager.E2E.Tests/Views/ShellWindow.cs
+++ Src/Desktop.Tests/ClinicManager.E2E.Tests/Views/ShellWindow.cs
@@ -2,5 +2,7 @@ namespace ClinicManager.E2E.Tests.Views;
 
 public class ShellWindow(FrameworkAutomationElementBase element) : Window(element)
 {
-  
+  public Button SendButton => this.Find("SendButton").AsButton();
+
+    
 }
