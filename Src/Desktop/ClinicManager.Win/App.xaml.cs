fab9c0496912ce84110fef67b31c5a7917bab690
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-24 15:13:18 +0300

    Update App.xaml.cs

--- Src/Desktop/ClinicManager.Win/App.xaml.cs
+++ Src/Desktop/ClinicManager.Win/App.xaml.cs
@@ -7,27 +7,9 @@ using Prism.Ioc;
 
 namespace ClinicManager.Win;
 
-public partial class App : PrismApplication
+public partial class App : CMApplication
 {
-    /// <summary>
-    /// Build the DryIoc container ourselves so we can pour an <see cref="IServiceCollection"/> into it
-    /// *before* Prism starts registering its own types. The returned container is then handed to Prism.
-    /// After this, there is ONE container: anything registered through Microsoft DI (IHttpClientFactory,
-    /// ILogger&lt;T&gt;, typed clients...) can be injected into Prism view models, and anything registered
-    /// through Prism (see <see cref="RegisterTypes"/>) can be injected into Microsoft DI-created objects
-    /// such as the HTTP logging handler.
-    /// </summary>
-    protected override IContainerExtension CreateContainerExtension()
-    {
-        var services = new ServiceCollection();
-        services.AddDefaultLogging();
-        services.AddHttpClients();
 
-        IContainer container = new DryIoc.Container(CreateContainerRules())
-            .WithDependencyInjectionAdapter(services);
-
-        return new DryIocContainerExtension(container);
-    }
 
     protected override Window CreateShell() => Container.Resolve<ShellWindow>();
 
@@ -38,13 +20,5 @@ public partial class App : PrismApplication
         containerRegistry.RegisterSingleton<IRequestLogService, RequestLogService>();
     }
 
-    protected override Rules CreateContainerRules()
-{
-    return Rules.Default.WithConcreteTypeDynamicRegistrations(reuse: Reuse.Transient)
-                        .With(Made.Of(FactoryMethod.ConstructorWithResolvableArguments))
-                        .WithFuncAndLazyWithoutRegistration()
-                        .WithTrackingDisposableTransients()
-                        //.WithoutFastExpressionCompiler()
-                        .WithFactorySelector(Rules.SelectLastRegisteredFactory());
-}
+
 }
