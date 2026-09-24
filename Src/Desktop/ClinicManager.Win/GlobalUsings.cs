378f8f71f007774a7734b2b423eef5fb5da95d81
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-24 09:58:08 +0300

    Add global using directives for ClinicManager.Win

---
+++ Src/Desktop/ClinicManager.Win/GlobalUsings.cs
@@ -0,0 +1,11 @@
+global using ClinicManager.Win.Features.Infrastructure.Shell;
+global using ClinicManager.Win.Features.Infrastructure;
+global using ClinicManager.Win.Features;
+global using ClinicManager.Win.Services;
+global using ClinicManager.Win.Ioc;
+global using ClinicManager.Win.Net.Http;
+
+
+
+
+
