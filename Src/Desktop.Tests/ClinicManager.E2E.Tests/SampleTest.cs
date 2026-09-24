b97cb90d976daaa071ab08e37b3e03044c793cf3
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-24 11:24:34 +0300

    Create SampleTest.cs

---
+++ Src/Desktop.Tests/ClinicManager.E2E.Tests/SampleTest.cs
@@ -0,0 +1,17 @@
+namespace ClinicManager.E2E.Tests
+{
+    
+    public class EmptyTests : UITestBase
+    {
+    
+        [Fact]
+        public void TestMethodName()
+        {
+            var shellWindow = GetShellWindow();
+            
+        }
+
+        
+        
+    }
+}
