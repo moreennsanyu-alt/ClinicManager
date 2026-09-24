7ab6ed70992e9782b82612a1d3c6ff483f4bf965
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-24 08:33:01 +0300

    Add global using directives for E2E tests

---
+++ Src/Desktop.Tests/ClinicManager.E2E.Tests/GlobalUsings.cs
@@ -0,0 +1,21 @@
+global using FlaUI.Core;
+global using FlaUI.Core.Capturing;
+global using FlaUI.Core.Input;
+global using FlaUI.Core.Tools;
+global using FlaUI.UIA3;
+global using FlaUI.Core.AutomationElements;
+global using FlaUI.Core.WindowsAPI;
+global using FlaUI.TestUtilities;
+global using FlaUI.Core.Definitions;
+
+global using Xunit;
+global using OperatingSystem = FlaUI.Core.Tools.OperatingSystem;
+global using ClinicManager.E2E.Tests.Core;
+global using ClinicManager.E2E.Tests.Core.Controls;
+global using ClinicManager.E2E.Tests.Core.SystemViews;
+global using ClinicManager.E2E.Tests.Views;
+global using static ThisAssembly.Constants;
+
+
+
+
