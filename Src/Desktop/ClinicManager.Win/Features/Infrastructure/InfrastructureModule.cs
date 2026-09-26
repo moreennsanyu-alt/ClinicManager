d61e97c115c4c2352172829d426305e5f64d4f56
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-09-26 08:10:30 +0300

    Register ShellWindow with ShellViewModel

--- Src/Desktop/ClinicManager.Win/Features/Infrastructure/InfrastructureModule.cs
+++ Src/Desktop/ClinicManager.Win/Features/Infrastructure/InfrastructureModule.cs
@@ -6,6 +6,8 @@ namespace ClinicManager.Win.Features.Infrastructure;
     {
         public void RegisterTypes(IContainerRegistry registry)
         {
+           ViewModelLocationProvider.Register<ShellWindow, ShellViewModel>();
+        
            // registry.RegisterSingleton<IOrderService, OrderService>();
            // registry.RegisterForNavigation<OrderListView, OrderListViewModel>();
         }
